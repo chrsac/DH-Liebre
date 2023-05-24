be73336fa97ef1cd50ff64273650562529e25977
chrsac <chrsac@gmail.com>
2023-05-24 20:25:27 -0300

    se actualiza path

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(express.static(__dirname+'/public'))
 app.listen(8000, () => console.log("Server en el puerto 3000"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/inicio.html'))
+    res.sendFile(path.join(__dirname, '/views/inicio.html'))
 })
 app.get('/404', (req, res) => {
     res.send('Error página no encontrada')
