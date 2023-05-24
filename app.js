8088a47d86fb5a78eb123fc141bc520650930b6a
chrsac <chrsac@gmail.com>
2023-05-24 20:20:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(__dirname+'/public'))
+
+app.listen(8000, () => console.log("Server en el puerto 3000"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/home.html'))
+})
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/inicio.html'))
+})
+app.get('/404', (req, res) => {
+    res.send('Error página no encontrada')
+})
