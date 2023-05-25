d0e33626d62c4cfaa5153b132d242a497dcb2115
chrsac <chrsac@gmail.com>
2023-05-24 21:21:23 -0300

    se agrega espacio en codigo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static(__dirname+'/public'))
 
-app.listen(process.env.PORT|| 3000, () => console.log("Server en el puerto 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("Server en el puerto 3000"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
