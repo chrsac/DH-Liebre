547b3c0c717680232a2c9f1d9c557c5e2ee35b88
chrsac <chrsac@gmail.com>
2023-05-24 21:03:00 -0300

    Se agrega info enviada por Lucas

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const app = express()
 
 app.use(express.static(__dirname+'/public'))
 
-app.listen(8000, () => console.log("Server en el puerto 3000"))
+app.listen(process.env.PORT|| 3000, () => console.log("Server en el puerto 3000"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/inicio.html'))
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 app.get('/404', (req, res) => {
     res.send('Error página no encontrada')
